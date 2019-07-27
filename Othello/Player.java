58e47b5ad11cd249987187187bf4ed1163880a25
YutaroM <yutaroapp@gmail.com>
2019-07-27 14:42:47 +0900

    day_32 ・Java学習（Othello） 　Playerクラスの追加

---
+++ Othello/Player.java
@@ -0,0 +1,13 @@
+/**
+ * @author Yutaro M
+ * @version 0.0.1
+ */
+public class Player {
+    boolean mTurn;
+    int mStones;
+
+   public Player(boolean _Turn, int _Stone) {
+       mTurn = _Turn;
+       mStones = _Stone;
+   }
+}
