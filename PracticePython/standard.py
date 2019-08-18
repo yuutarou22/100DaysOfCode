f20c55a32c96a04f1683e06f25a0f548f17d1f68
YutaroM <yutaroapp@gmail.com>
2019-08-19 08:18:04 +0900

    day37_Python学習

---
+++ PracticePython/standard.py
@@ -0,0 +1,13 @@
+msg = "Hello"
+print msg
+
+print 10 * 5 #50
+print 10 // 3 #3 整数の商
+print 10 % 3 #1 割った余り
+print 2**3 #8 2*2*2をする
+
+# """で改行も考慮する
+print """<html lnag="ja">
+<body>
+</body>
+</html>"""
