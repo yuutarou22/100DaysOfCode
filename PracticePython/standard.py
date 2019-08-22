282da4adb8782077ba0f412778382b4edcefcdab
YutaroM <yutaroapp@gmail.com>
2019-08-22 23:49:40 +0900

    day_40 Python学習 Kivyの環境構築（まだできない。。）

--- PracticePython/standard.py
+++ PracticePython/standard.py
@@ -5,5 +5,18 @@ print ("Hello, World")
 # 関数
 def inputName(name):
     print ("よろしくお願いします、" + name + " さん！")
-    
+
 inputName(input("name = "))
+
+while True:
+    inputYear = int(input("西暦> "))
+
+    if inputYear % 400:
+        print (str(inputYear) + "年は閏年です")
+    elif inputYear % 100:
+        print (str(inputYear) + "年は平年です")
+    elif inputYear % 4:
+        print (str(inputYear) + "年は閏年です")
+    else:
+        print (str(inputYear) + "年は平年です")
+
